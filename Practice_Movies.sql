de27688e4e434ea0832ff0514c3dd1482a33fdce
OlgaKhudnitskaya <105886502+OlgaKhudnitskaya@users.noreply.github.com>
2022-12-27 14:58:13 +0300

    Add 7 tasks

--- Practice_Movies.sql
+++ Practice_Movies.sql
@@ -4,3 +4,45 @@
 -- casting: хранится информация для связи фильма с актерами: идентификатор фильма (movieid), идентификатор актера (actorid), роль актера (ord).
 --*Если ord = 1 - значит актер игает главную роль
 
+--Напишите запрос, который вернёт все фильмы за 1962 год.
+SELECT * FROM movie
+WHERE yr=1962;
+
+--Напишите запрос, который вернёт год (yr) фильма под названием Citizen Kane .
+SELECT yr FROM movie
+WHERE title='Citizen Kane'
+
+--Напишите запрос, который вернёт id, title и yr всех фильмов про Star Trek. Результаты должны быть отсортированы по году выпуска фильма.
+--Используйте оператор LIKE.
+SELECT id, title, yr FROM movie
+WHERE title LIKE '%Star Trek%'
+ORDER BY yr;
+
+--Напишите запрос, который вернёт id актера Glenn Close
+SELECT id FROM actor
+WHERE name='Glenn Close';
+
+--Напишите запрос, который вернёт id фильма Casablanca.
+SELECT id FROM movie
+WHERE title='Casablanca';
+
+--Напишите запрос, который вернёт всех актеров из кастинга фильма Schindlers List.
+--Используйте оператор JOIN для таблиц casting и actor.
+--Bспользуйте параметр movieid = 7
+SELECT name FROM casting 
+JOIN actor
+ON id=actorid
+WHERE movieid = 7;
+
+--Напишите запрос, который вернёт список актеров из кастинга на фильм Alien
+--Используйте оператор JOIN два раза для таблиц casting и actor, а так же casting и movie
+SELECT name FROM casting 
+JOIN actor
+ON actorid=actor.id
+JOIN movie
+ON movieid=movie.id
+WHERE title = 'Alien'
+
+
+
+
